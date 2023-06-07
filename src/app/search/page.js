1720917c07cee583e5f52d5135b74f11ecdebe37
Oliver Roick <oliver@roick.email>
2023-06-07 16:46:49 +1000

    feat: Clip or pad audio before upload

--- src/app/search/page.js
+++ src/app/search/page.js
@@ -1,12 +1,9 @@
 'use client';
-import { useState, useEffect} from 'react';
 import dynamic from 'next/dynamic';
 import { Box, Button, Container, Heading } from '@chakra-ui/react';
 
 import { Loading } from '@/components';
 import { InpageHeader } from '@/components/page';
-import { MAX_AUDIO_CLIP_LENGTH } from '@/settings';
-import getDuration from '@/utils/getDuration';
 
 import AudioSelectForm from './AudioSelectForm';
 import useSearchForm from './hooks/useSearchForm';
@@ -15,33 +12,15 @@ const AudioClipper = dynamic(() => import('./AudioClipper'), {
   loading: () => <Loading size="xl" />,
 });
 
-export default function Upload() {
-  const [clipStart, setClipStart] = useState();
-  const [clipLength, setClipLength] = useState(); // eslint-disable-line
-  const [ duration, setDuration ] = useState();
-
-  
+export default function Upload() { 
   const {
     file,
     setFile,
     results,
     isSubmitting,
-    handleFormSubmit
+    submitButtonProps,
+    setClip
   } = useSearchForm();
-  
-  const setClip = (start, length) => {
-    setClipStart(start);
-    setClipLength(length);
-  };
-  
-  useEffect(() => {
-    if (file) {
-      getDuration(file).then(setDuration);
-    } else { 
-      setDuration();
-    }
-  }, [file]);
-  const disableSubmit = isSubmitting || (duration > MAX_AUDIO_CLIP_LENGTH && !clipStart);
 
   return (
     <main>
@@ -60,8 +39,7 @@ export default function Upload() {
               <Button
                 variant="primary"
                 type="submit"
-                onClick={handleFormSubmit}
-                isDisabled={disableSubmit}
+                {...submitButtonProps}
               >
                 Search
               </Button>
