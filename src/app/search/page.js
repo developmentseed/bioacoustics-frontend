8ad4a3f1a3399d4fe25fa26661456893af648e07
Oliver Roick <oliver@roick.email>
2023-06-07 12:03:22 +1000

    feat: Prevent submitting before clipping

--- src/app/search/page.js
+++ src/app/search/page.js
@@ -1,10 +1,13 @@
 'use client';
-import { useState } from 'react';
+import { useState, useEffect} from 'react';
 import dynamic from 'next/dynamic';
 import { Box, Button, Container, Heading } from '@chakra-ui/react';
 
 import { Loading } from '@/components';
 import { InpageHeader } from '@/components/page';
+import { MAX_AUDIO_CLIP_LENGTH } from '@/settings';
+import getDuration from '@/utils/getDuration';
+
 import AudioSelectForm from './AudioSelectForm';
 import useSearchForm from './hooks/useSearchForm';
 import Results from './results';
@@ -13,8 +16,11 @@ const AudioClipper = dynamic(() => import('./AudioClipper'), {
 });
 
 export default function Upload() {
-  const [clipStart, setClipStart] = useState(); // eslint-disable-line
+  const [clipStart, setClipStart] = useState();
   const [clipLength, setClipLength] = useState(); // eslint-disable-line
+  const [ duration, setDuration ] = useState();
+
+  
   const {
     file,
     setFile,
@@ -22,11 +28,20 @@ export default function Upload() {
     isSubmitting,
     handleFormSubmit
   } = useSearchForm();
-
+  
   const setClip = (start, length) => {
     setClipStart(start);
     setClipLength(length);
   };
+  
+  useEffect(() => {
+    if (file) {
+      getDuration(file).then(setDuration);
+    } else { 
+      setDuration();
+    }
+  }, [file]);
+  const disableSubmit = isSubmitting || (duration > MAX_AUDIO_CLIP_LENGTH && !clipStart);
 
   return (
     <main>
@@ -46,7 +61,7 @@ export default function Upload() {
                 variant="primary"
                 type="submit"
                 onClick={handleFormSubmit}
-                isDisabled={isSubmitting}
+                isDisabled={disableSubmit}
               >
                 Search
               </Button>
--- src/settings.js
+++ src/settings.js
@@ -1,4 +1,4 @@
-const ACCEPTED_AUDIO_TYPES = ['.mp3', '.wav', '.flac'];
+const ACCEPTED_AUDIO_TYPES = ['.mp3', '.wav', '.flac', '.m4a'];
 const MAX_AUDIO_CLIP_LENGTH = 5; // seconds
 const MAX_AUDIO_SIZE = 1073741824; // 1GB
 const MAX_AUDIO_LENGTH = 300; // seconds
