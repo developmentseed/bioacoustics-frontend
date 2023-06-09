36da08d6884410328d46e732c210f853274142ed
LanesGood <Lane@developmentseed.org>
2023-06-09 12:06:38 -0400

    Update grid card style

--- src/app/search/results/GridView.jsx
+++ src/app/search/results/GridView.jsx
@@ -1,5 +1,5 @@
 import T from 'prop-types';
-import { Button, Card, CardBody, Grid, GridItem, IconButton, Image, Link, Text } from '@chakra-ui/react';
+import { Box, Button, Card, CardBody, Grid, GridItem, IconButton, Image, Link } from '@chakra-ui/react';
 import { MdOpenInNew } from 'react-icons/md';
 
 import { TMatch } from '@/types';
@@ -13,37 +13,40 @@ function ResultCard({ result, large }) {
   const audioUrl = getAudioUrlfromImageUrl(image_url);
 
   return (
-    <Card size="sm" fontSize="sm" data-testid="result-card">
-      <CardBody as={Grid} gap={2} templateRows={gridConfig}>
-        <Grid gap="1" templateColumns="1fr min-content">
-          <Text textOverflow="ellipsis" overflow="hidden" whiteSpace="nowrap">{ filename.split('/')[1] }</Text>
+    <Card size="sm" fontSize="xs" data-testid="result-card">
+      <CardBody as={Grid} gap={2} templateRows={gridConfig} p={0}>
+        <Box position="absolute" right={1} top={1} zIndex={10}>
           <AudioPlayer audioSrc={audioUrl} />
-        </Grid>
-        <Image
-          src={image_url}
-          alt={`Spectrogram for match from ${filename}`}
-          loading="lazy"
-          fit="fill"
-          htmlHeight="256"
-          htmlWidth="215"
-          height="100"
-          width="100%"
-        />
+        </Box>
+        <Box 
+          position="relative"
+        >
+          <Image
+            src={image_url}
+            alt={`Spectrogram for match from ${filename}`}
+            loading="lazy"
+            fit="fill"
+            htmlHeight="256"
+            htmlWidth="215"
+            height="100"
+            width="100%"
+          />
+        </Box>
         {large && (
-          <Grid templateColumns="min-content 1fr" gap="1">
-            <GridItem><b>Site:</b></GridItem>
+          <Grid templateColumns="min-content 1fr" gap="1" px={2}>
+            <GridItem fontWeight="bold" color="neutral.400">Site:</GridItem>
             <GridItem>{ sitenameDisplay(result) }</GridItem>
-            <GridItem><b>Recording time:</b></GridItem>
+            <GridItem fontWeight="bold" color="neutral.400">Recorded:</GridItem>
             <GridItem>{ formatDate(file_timestamp) }</GridItem>
-            <GridItem><b>Result time:</b></GridItem>
+            <GridItem fontWeight="bold" color="neutral.400">Result:</GridItem>
             <GridItem>{ formatDate(file_timestamp + clip_offset_in_file) }</GridItem>
           </Grid>
         )}
-        <GridItem alignSelf="end">
+        <GridItem alignSelf="end" px={2}>
           {large ? (
-            <Button as={Link} variant="link" href={audioUrl} target="_blank" rightIcon={<MdOpenInNew />} size="sm">Full Recording</Button>
+            <Button as={Link} variant="link" href={audioUrl} target="_blank" rightIcon={<MdOpenInNew />} size="xs">Full Recording</Button>
           ): (
-            <IconButton as={Link} variant="link" href={audioUrl} target="_blank" icon={<MdOpenInNew />} size="sm" title="Full Recording" display="inline" />
+            <IconButton as={Link} variant="link" href={audioUrl} target="_blank" icon={<MdOpenInNew />} size="xs" title="Full Recording" display="inline" />
           )}
         </GridItem>
       </CardBody>
