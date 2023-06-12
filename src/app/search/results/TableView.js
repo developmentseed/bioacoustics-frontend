fc5e16b4d482789863900042363ee43c83d0e72e
LanesGood <Lane@developmentseed.org>
2023-06-12 14:40:55 -0400

    Add audioplayer and external link to table rows

--- src/app/search/results/TableView.js
+++ src/app/search/results/TableView.js
@@ -7,19 +7,30 @@ import {
   Th,
   Td,
   TableContainer,
+  IconButton,
+  Link,
 } from '@chakra-ui/react';
+import { MdOpenInNew } from 'react-icons/md';
 import { TMatch } from '@/types';
 import { formatDate } from '@/utils';
-import { sitenameDisplay } from './utils';
+import { sitenameDisplay, getAudioUrlfromImageUrl } from './utils';
+import AudioPlayer from './AudioPlayer';
 
 function ResultRow({ result }) {
-  const { id, distance, entity: { file_timestamp } } = result;
+  const { distance, entity: { file_timestamp, file_seq_id, image_url } } = result;
+  const audioUrl = getAudioUrlfromImageUrl(image_url);
+
   return (
     <Tr>
-      <Td>{ id }</Td>
-      <Td>{ distance }</Td>
+      <Td>
+        <AudioPlayer audioSrc={audioUrl} />
+      </Td>
+      <Td>{ distance.toFixed(4) }</Td>
       <Td>{ sitenameDisplay(result) }</Td>
       <Td>{ formatDate(file_timestamp) }</Td>
+      <Td>
+        <IconButton as={Link} variant="link" href={`https://data.acousticobservatory.org/listen/${file_seq_id}`} target="_blank" icon={<MdOpenInNew />} size="sm" title="Full Recording" display="inline" />
+      </Td>
     </Tr>
   );
 }
@@ -30,14 +41,15 @@ ResultRow.propTypes = {
 
 export default function TableView({ results }) {
   return (
-    <TableContainer>
-      <Table variant="simple">
+    <TableContainer bg="white" boxShadow="base" borderRadius={4}>
+      <Table size="sm">
         <Thead>
           <Tr>
-            <Th>ID</Th>
-            <Th>Distance</Th>
-            <Th>Site</Th>
-            <Th>Timestamp</Th>
+            <Th />
+            <Th py={2} color="blackAlpha.600">Distance</Th>
+            <Th py={2} color="blackAlpha.600">Site</Th>
+            <Th py={2} color="blackAlpha.600">Timestamp</Th>
+            <Th />
           </Tr>
         </Thead>
         <Tbody data-testid="results-table">
