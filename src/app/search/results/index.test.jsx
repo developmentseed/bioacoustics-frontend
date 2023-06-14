6efba32a5637d85825a0723197bbe542c345f7ea
Oliver Roick <oliver@roick.email>
2023-06-14 12:27:36 +1000

    test: Fix tests

--- src/app/search/results/index.test.jsx
+++ src/app/search/results/index.test.jsx
@@ -10,28 +10,14 @@ describe('Results', () => {
     expect(screen.queryByTestId('loading')).toBeInTheDocument();
   });
 
-  it('renders the results table', () => {
-    render(
-      <Results
-        isLoading={false}
-        results={results}
-      />);
-    expect(screen.queryByTestId('loading')).not.toBeInTheDocument();
-    expect(screen.queryByTestId('results-table')).toBeInTheDocument();
-  });
-
   it('renders the small results grid', async () => {
     render(
       <Results
         isLoading={false}
         results={results}
       />);
-    expect(screen.queryByTestId('results-table')).toBeInTheDocument();
-    expect(screen.queryByTestId('results-grid')).not.toBeInTheDocument();
-
-    userEvent.click(screen.getByLabelText('View results in small grid'));
-    expect(await screen.findByTestId('results-grid')).toBeInTheDocument();
     expect(screen.queryByTestId('results-table')).not.toBeInTheDocument();
+    expect(screen.queryByTestId('results-grid')).toBeInTheDocument();
   });
 
   it('renders the large results grid', async () => {
@@ -40,11 +26,25 @@ describe('Results', () => {
         isLoading={false}
         results={results}
       />);
-    expect(screen.queryByTestId('results-table')).toBeInTheDocument();
-    expect(screen.queryByTestId('results-grid')).not.toBeInTheDocument();
+    expect(screen.queryByTestId('results-table')).not.toBeInTheDocument();
+    expect(screen.queryByTestId('results-grid')).toBeInTheDocument();
 
     userEvent.click(screen.getByLabelText('View results in large grid'));
     expect(await screen.findByTestId('results-grid')).toBeInTheDocument();
     expect(screen.queryByTestId('results-table')).not.toBeInTheDocument();
+  });
+
+  it('renders the results table', async () => {
+    render(
+      <Results
+        isLoading={false}
+        results={results}
+      />);
+    expect(screen.queryByTestId('loading')).not.toBeInTheDocument();
+    expect(screen.queryByTestId('results-table')).not.toBeInTheDocument();
+
+    userEvent.click(screen.getByLabelText('View results in table'));
+    expect(await screen.findByTestId('results-table')).toBeInTheDocument();
+    expect(screen.queryByTestId('results-grid')).not.toBeInTheDocument();
   });
 });
