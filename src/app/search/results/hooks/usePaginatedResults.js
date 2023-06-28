0c48fca33c6fab6b8fec230f46733910f749ca89
Oliver Roick <oliver@roick.email>
2023-06-28 11:38:51 +1000

    feat: Clear selected results on filter change

--- src/app/search/results/hooks/usePaginatedResults.js
+++ src/app/search/results/hooks/usePaginatedResults.js
@@ -38,6 +38,7 @@ export default function usePaginatedResults(results) {
     page,
     resultPage,
     numMatches,
+    selectedSites,
     setSelectedSites,
     selectedDates,
     setSelectedDates,
