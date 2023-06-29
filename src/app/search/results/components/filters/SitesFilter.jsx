14279d9ea0fd5e816264a9b8a03a7a8f7b05b930
Oliver Roick <oliver@roick.email>
2023-06-29 16:33:13 +1000

    feat: Display sites and enable site selected on map

--- src/app/search/results/components/filters/SitesFilter.jsx
+++ src/app/search/results/components/filters/SitesFilter.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useRef, useState, useMemo } from 'react';
 import T from 'prop-types';
 import { MdKeyboardArrowDown } from 'react-icons/md';
 import {
@@ -20,13 +20,49 @@ import {
   useDisclosure
 } from '@chakra-ui/react';
 import { MdMap } from 'react-icons/md';
+import Map, { Source, Layer} from 'react-map-gl';
 
+import { MAPBOX_TOKEN } from '@/settings';
 import { useSites } from '../../../context/sites';
+
+const markerStyle = {
+  id: 'sites',
+  type: 'circle',
+  paint: {
+    'circle-radius': 5,
+    'circle-color': [
+      'case',
+      ['==', ['get', 'isSelected'], true],
+      '#037447',
+      '#fff',
+    ],
+    'circle-stroke-color': '#7EC440',
+    'circle-stroke-width': 2
+  }
+};
 
 export default function SitesFilter({ selectedSites, setSelectedSites }) {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [siteNameFilter, setSiteNameFilter] = useState('');
+  const mapRef = useRef();
   const { sites } = useSites();
+
+  const geojson = useMemo(() => {
+    return ({
+      type: 'FeatureCollection',
+      features: sites.map(({ id, custom_latitude, custom_longitude }) => {
+        return ({
+          type: 'Feature',
+          id,
+          geometry: { type: 'Point', coordinates: [custom_longitude, custom_latitude] },
+          properties: {
+            id,
+            isSelected: selectedSites.includes(id)
+          }
+        });
+      })
+    });
+  }, [selectedSites, sites]);
 
   const handleSiteSelect = useCallback((checked) => {
     setSelectedSites(checked.map(id => parseInt(id, 10)));
@@ -36,6 +72,39 @@ export default function SitesFilter({ selectedSites, setSelectedSites }) {
     (e) => setSiteNameFilter(e.target.value),
     []
   );
+
+  const toggleSiteSelect = useCallback((id) => {
+    if (selectedSites.includes(id)) {
+      setSelectedSites(selectedSites.filter(site => site !== id));
+    } else {
+      setSelectedSites([...selectedSites, id]);
+    }
+  }, [selectedSites, setSelectedSites]);
+
+  const handleMarkerClick = useCallback((e) => {
+    const map = mapRef.current;
+    const features = map.queryRenderedFeatures(e.point, {
+      layers: [markerStyle.id],
+    });
+    if (!features.length > 0) return;
+
+    const clusterId = features[0].properties.cluster_id;
+    if (clusterId) {
+      // Clicked on a cluster marker, zoom in
+      map
+        .getSource('sites')
+        .getClusterExpansionZoom(clusterId, (err, zoom) => {
+          if (err) return;
+          map.easeTo({
+            center: features[0].geometry.coordinates,
+            zoom: zoom,
+          });
+        });
+    } else {
+      // click on a site, toggle selection
+      toggleSiteSelect(features[0].id);
+    }
+  }, [toggleSiteSelect]);
 
   return (
     <Popover placement="bottom-start">
@@ -71,7 +140,30 @@ export default function SitesFilter({ selectedSites, setSelectedSites }) {
                     <Heading as="h3" size="md" color="primary.500">Select sites</Heading>
                   </Box>
                   <Box height="500px" bgColor="neutral.100">
-                    Map
+                    <Map
+                      initialViewState={{
+                        longitude: 134.396315,
+                        latitude: -26.7302804,
+                        zoom: 3
+                      }}
+                      mapStyle="mapbox://styles/mapbox/light-v11"
+                      mapboxAccessToken={MAPBOX_TOKEN}
+                      ref={mapRef}
+                      interactiveLayerIds={[markerStyle.id]}
+                      onClick={handleMarkerClick}
+                    >
+                      <Source
+                        id="sites"
+                        type="geojson"
+                        data={geojson}
+                        cluster={true}
+                        clusterMaxZoom={14}
+                        clusterRadius={30}
+                        clusterProperties={{ clusterNumResults: ['+', ['get', 'numResults']] }}
+                      >
+                        <Layer {...markerStyle} />
+                      </Source>
+                    </Map>
                   </Box>
                 </ModalBody>
               </ModalContent>
