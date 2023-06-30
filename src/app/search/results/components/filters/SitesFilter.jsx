52522721271015cc82f6a4efc0d4f355cff91fc8
Oliver Roick <oliver@roick.email>
2023-06-30 13:13:33 +1000

    refactor: Move select map to separate component

--- src/app/search/results/components/filters/SitesFilter.jsx
+++ src/app/search/results/components/filters/SitesFilter.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useRef, useState, useMemo, useEffect } from 'react';
+import { useCallback, useState } from 'react';
 import T from 'prop-types';
 import {
   Box,
@@ -21,95 +21,15 @@ import {
   useDisclosure
 } from '@chakra-ui/react';
 import { MdClose, MdKeyboardArrowDown, MdMap, MdDraw } from 'react-icons/md';
-import Map, { Source, Layer, Popup, useControl } from 'react-map-gl';
-import MapboxDraw from '@mapbox/mapbox-gl-draw';
-import pointsWithinPolygon from '@turf/points-within-polygon';
 
-import { MAPBOX_TOKEN } from '@/settings';
 import { useSites } from '../../../context/sites';
-
-const markerStyle = {
-  id: 'sites',
-  type: 'circle',
-  paint: {
-    'circle-radius': 5,
-    'circle-color': [
-      'case',
-      ['==', ['get', 'isSelected'], true],
-      '#037447',
-      '#fff',
-    ],
-    'circle-stroke-color': '#7EC440',
-    'circle-stroke-width': 2
-  }
-};
-
-function DrawControl(props) {
-  const { isEnabled, selectedSites, onComplete } = props;
-
-  const { changeMode, deleteAll, modes } = useControl(
-    () => new MapboxDraw(props),
-    ({ map }) => {
-      map.on('draw.create', onComplete);
-      map.on('draw.update', onComplete);
-    },
-    ({ map }) => {
-      map.off('draw.create', onComplete);
-      map.off('draw.update', onComplete);
-    },
-    {
-      position: props.position
-    }
-  );
-
-  useEffect(() => {
-    if (isEnabled) {
-      deleteAll();
-      changeMode(modes.DRAW_POLYGON);
-    }
-  }, [changeMode, deleteAll, isEnabled, modes]);
-
-  useEffect(() => {
-    if (selectedSites.length === 0) {
-      deleteAll();
-    }
-  }, [deleteAll, selectedSites]);
-
-  return null;
-}
-
-DrawControl.propTypes = {
-  isEnabled: T.bool,
-  selectedSites: T.arrayOf(T.number).isRequired,
-  position: T.string,
-  onComplete: T.func.isRequired
-};
+import SitesFilterMap from './SitesFilterMap';
 
 export default function SitesFilter({ selectedSites, setSelectedSites }) {
-  const mapRef = useRef();
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [siteNameFilter, setSiteNameFilter] = useState('');
-  const [hoveredSite, setHoveredSite] = useState();
   const [isDrawing, setIsDrawing] = useState();
   const { sites } = useSites();
-
-  const geojson = useMemo(() => {
-    return ({
-      type: 'FeatureCollection',
-      features: sites.map(({ id, name, custom_latitude, custom_longitude }) => {
-        return ({
-          type: 'Feature',
-          id,
-          geometry: { type: 'Point', coordinates: [custom_longitude, custom_latitude] },
-          properties: {
-            id,
-            name,
-            isSelected: selectedSites.includes(id)
-          }
-        });
-      })
-    });
-  }, [selectedSites, sites]);
 
   const handleSiteSelect = useCallback((checked) => {
     setSelectedSites(checked.map(id => parseInt(id, 10)));
@@ -119,65 +39,6 @@ export default function SitesFilter({ selectedSites, setSelectedSites }) {
     (e) => setSiteNameFilter(e.target.value),
     []
   );
-
-  const toggleSiteSelect = useCallback((id) => {
-    if (selectedSites.includes(id)) {
-      setSelectedSites(selectedSites.filter(site => site !== id));
-    } else {
-      setSelectedSites([...selectedSites, id]);
-    }
-  }, [selectedSites, setSelectedSites]);
-
-  const handleMarkerClick = useCallback((e) => {
-    const map = mapRef.current;
-    const features = map.queryRenderedFeatures(e.point, {
-      layers: [markerStyle.id],
-    });
-    if (!features.length > 0) return;
-
-    const clusterId = features[0].properties.cluster_id;
-    if (clusterId) {
-      // Clicked on a cluster marker, zoom in
-      map
-        .getSource('sites')
-        .getClusterExpansionZoom(clusterId, (err, zoom) => {
-          if (err) return;
-          map.easeTo({
-            center: features[0].geometry.coordinates,
-            zoom: zoom,
-          });
-        });
-    } else {
-      // click on a site, toggle selection
-      toggleSiteSelect(features[0].id);
-    }
-  }, [toggleSiteSelect]);
-
-  const handleMarkerHover = (e) => {
-    const map = mapRef.current;
-    const features = map.queryRenderedFeatures(e.point, {
-      layers: [markerStyle.id],
-    });
-
-    if (features.length === 0) {
-      setHoveredSite();
-      return;
-    }
-
-    const { name, cluster_id } = features[0].properties;
-    const [ longitude, latitude ] = features[0].geometry.coordinates;
-    if (!cluster_id) {
-      setHoveredSite({ name, longitude, latitude });
-    } else {
-      setHoveredSite();
-    }
-  };
-
-  const handleDrawChange = useCallback(({ features }) => {
-    setIsDrawing(false);
-    const sitesInArea = pointsWithinPolygon(geojson, features[0]);
-    setSelectedSites(sitesInArea.features.map(({ id }) => id));
-  }, [geojson, setSelectedSites]);
 
   return (
     <Popover placement="bottom-start">
@@ -216,52 +77,12 @@ export default function SitesFilter({ selectedSites, setSelectedSites }) {
                     <Button variant="outline" leftIcon={<MdClose />} size="xs" onClick={onClose}>Close</Button>
                   </Flex>
                   <Box height="500px" bgColor="neutral.100">
-                    <Map
-                      initialViewState={{
-                        longitude: 134.396315,
-                        latitude: -26.7302804,
-                        zoom: 3
-                      }}
-                      mapStyle="mapbox://styles/mapbox/light-v11"
-                      mapboxAccessToken={MAPBOX_TOKEN}
-                      ref={mapRef}
-                      interactiveLayerIds={[markerStyle.id]}
-                      onClick={handleMarkerClick}
-                      onMouseMove={handleMarkerHover}
-                    >
-                      <Source
-                        id="sites"
-                        type="geojson"
-                        data={geojson}
-                        cluster={true}
-                        clusterMaxZoom={14}
-                        clusterRadius={30}
-                        clusterProperties={{ clusterNumResults: ['+', ['get', 'numResults']] }}
-                      >
-                        <Layer {...markerStyle} />
-                      </Source>
-                      {hoveredSite && (
-                        <Popup
-                          anchor="bottom"
-                          longitude={Number(hoveredSite.longitude)}
-                          latitude={Number(hoveredSite.latitude)}
-                          closeButton={false}
-                        >
-                          { hoveredSite.name }
-                        </Popup>
-                      )}
-                      <DrawControl
-                        position="top-left"
-                        displayControlsDefault={false}
-                        selectedSites={selectedSites}
-                        isEnabled={isDrawing}
-                        onComplete={handleDrawChange}
-                        // controls={{
-                        //   polygon: true,
-                        //   trash: true
-                        // }}
-                      />
-                    </Map>
+                    <SitesFilterMap
+                      selectedSites={selectedSites}
+                      setSelectedSites={setSelectedSites}
+                      isDrawing={isDrawing}
+                      setIsDrawing={setIsDrawing}
+                    />
                   </Box>
                 </ModalBody>
               </ModalContent>
