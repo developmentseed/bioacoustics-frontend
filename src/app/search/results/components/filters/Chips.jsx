1757c3a91cd9cba2fb00be622c30e48bab525f5d
Oliver Roick <oliver@roick.email>
2023-07-05 11:49:40 +1000

    chore: Refactor common chip markup into component

--- src/app/search/results/components/filters/Chips.jsx
+++ src/app/search/results/components/filters/Chips.jsx
@@ -4,6 +4,25 @@ import { Box, Tag, TagLabel, TagCloseButton } from '@chakra-ui/react';
 import { formatDate } from '@/utils';
 import formatHour from './formatHour';
 
+function Chip({ children, onClear }) {
+  return (
+    <Tag
+      size="sm"
+      borderRadius="full"
+      variant="solid"
+      bgColor="primary.400"
+    >
+      <TagLabel>{children}</TagLabel>
+      <TagCloseButton onClick={onClear} />
+    </Tag>
+  );
+}
+
+Chip.propTypes = {
+  children: T.node.isRequired,
+  onClear: T.func.isRequired
+};
+
 export default function Chips ({
   selectedSites,
   setSelectedSites,
@@ -15,37 +34,17 @@ export default function Chips ({
   return (
     <Box>
       { selectedSites.length > 0 && (
-        <Tag
-          size="sm"
-          borderRadius="full"
-          variant="solid"
-          bgColor="primary.400"
-        >
-          <TagLabel>Sites ({selectedSites.length})</TagLabel>
-          <TagCloseButton onClick={() => setSelectedSites([])} />
-        </Tag>
+        <Chip onClear={() => setSelectedSites([])}>Sites ({selectedSites.length})</Chip>
       )}
       { selectedDates.length > 0 && (
-        <Tag
-          size="sm"
-          borderRadius="full"
-          variant="solid"
-          bgColor="primary.400"
-        >
-          <TagLabel>{formatDate(selectedDates[0])} {selectedDates[1] && `- ${formatDate(selectedDates[1])}`}</TagLabel>
-          <TagCloseButton onClick={() => setSelectedDates([])} />
-        </Tag>
+        <Chip onClear={() => setSelectedDates([])}>
+          {formatDate(selectedDates[0])} {selectedDates[1] && `- ${formatDate(selectedDates[1])}`}
+        </Chip>
       )}
       { (selectedTimes[0] !== 0 || selectedTimes[1] !== 24) && (
-        <Tag
-          size="sm"
-          borderRadius="full"
-          variant="solid"
-          bgColor="primary.400"
-        >
-          <TagLabel>{formatHour(selectedTimes[0])} - {formatHour(selectedTimes[1])} UTC</TagLabel>
-          <TagCloseButton onClick={() => setSelectedTimes([0, 24])} />
-        </Tag>
+        <Chip onClear={() => setSelectedTimes([0, 24])}>
+          {formatHour(selectedTimes[0])} - {formatHour(selectedTimes[1])} UTC
+        </Chip>
       )}
     </Box>
   );
