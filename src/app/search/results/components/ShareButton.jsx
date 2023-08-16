f5e57dc5f60e32c7650b88d956c5845ae609d912
Oliver Roick <oliver@roick.email>
2023-08-16 12:49:46 +1000

    feat: Update button state

--- src/app/search/results/components/ShareButton.jsx
+++ src/app/search/results/components/ShareButton.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import T from 'prop-types';
 import { Button } from '@chakra-ui/react';
 import { useSearchParams } from 'next/navigation';
@@ -19,33 +20,46 @@ const keys = [
 export default function ShareButton({ file }) {
   const appState = useAppState();
   const searchParams = useSearchParams();
+  const [ buttonLabel, setButtonLabel ] = useState('Share this page');
+  const [ isDisabled, setIsDisabled ] = useState(false);
 
   const handleShare = () => {
-    const fileUrl = searchParams.get('q');
-    const { protocol, host } = window.location;
+    setIsDisabled(true);
+    new Promise((resolve) => {
+      const fileUrl = searchParams.get('q');
+      const { protocol, host } = window.location;
+      if (fileUrl) {
+        resolve(`${protocol}//${host}/search?q=${encodeURIComponent(fileUrl)}&${appState.urlEncode(keys)}`);
+        return;
+      }
 
-    if (fileUrl) {
-      const shareUrl = `${protocol}//${host}/search?q=${encodeURIComponent(fileUrl)}&${appState.urlEncode(keys)}`;
-      navigator.clipboard.writeText(shareUrl);
-    } else {
+      setButtonLabel('Uploading audio');
       const formData = new FormData();
       formData.append('audio_file', file);
   
-      return fetch(`${SEARCH_API}/upload-audio/`, {
+      fetch(`${SEARCH_API}/upload-audio/`, {
         method: 'POST',
         body: formData,
       })
         .then(r => r.json())
         .then(({ bucket_name, filename }) => {
           const fileUrl = `http://${bucket_name}.storage.googleapis.com/${filename}`;
-          const shareUrl = `${protocol}//${host}/search?q=${fileUrl}&${appState.urlEncode(keys)}`;
-          navigator.clipboard.writeText(shareUrl);
+          resolve(`${protocol}//${host}/search?q=${fileUrl}&${appState.urlEncode(keys)}`);
         });
-    }
+
+    })
+      .then(shareUrl => navigator.clipboard.writeText(shareUrl))
+      .then(() => {
+        setButtonLabel('URL copied to clipboard');
+        setTimeout(() => {
+          setButtonLabel('Share this page');
+          setIsDisabled(false);
+        }, 5000);
+      });
   };
 
   return (
-    <Button variant="primary" size="sm" onClick={handleShare}>Share this page</Button>
+    <Button variant="primary" size="sm" onClick={handleShare} isDisabled={isDisabled}>{ buttonLabel }</Button>
   );
 }
 
