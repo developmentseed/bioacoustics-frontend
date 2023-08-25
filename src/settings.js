d97e0f55dbe852404226915972f65fee95c36160
LanesGood <Lane@developmentseed.org>
2023-08-25 17:40:01 -0400

    Use 24 items as default page size

--- src/settings.js
+++ src/settings.js
@@ -10,7 +10,7 @@ const MAX_AUDIO_CLIP_LENGTH = 5; // seconds
 const MAX_AUDIO_SIZE = 1073741824; // 1GB
 const MAX_AUDIO_LENGTH = 300; // seconds
 const RESULTS_MAX = 16384;
-const RESULTS_DISPLAY_PAGE_SIZE = 25;
+const RESULTS_DISPLAY_PAGE_SIZE = 24;
 const SEARCH_API = 'https://api.bioacoustics.ds.io/api/v1';
 const MAPBOX_TOKEN = 'pk.eyJ1IjoiZGV2c2VlZCIsImEiOiJjbGpmOG5wOG8yNXVsM29wbTZkdWh2dnczIn0.hbIHh4NVESSlh6JKH3CMlw';
 
